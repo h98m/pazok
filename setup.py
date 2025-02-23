40b0f246b568cf388412f0c39ccd6a3a2dc60f3a
aamya2003 <ahmadasaadmajed1@gmail.com>
2025-02-23 19:15:06 +0300

    ew func added to module

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='pazok',
-    version='0.1.4.1',
+    version='0.1.4.2',
     author='b_azo',
     packages=find_packages(),
     install_requires=[

11625791af708771eef234dad3fd3b1a0afab703
Evil / شـر <164701255+bazok11@users.noreply.github.com>
2025-03-20 18:19:55 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='pazok',
-    version='0.2.0.8',
+    version='0.2.0.9',
     author='b_azo',
     packages=find_packages(),
     long_description=open('README.md').read(),

08722c320daca700137f681a4fd97ceb50b573ee
HUSSEIN HAFEDH OBAID <100138589+h98m@users.noreply.github.com>
2025-08-21 19:22:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='pazok',
-    version='0.2.2.1',
+    version='0.2.2.0',
     author='b_azo',
     packages=find_packages(),
     long_description=open('README.md').read(),
